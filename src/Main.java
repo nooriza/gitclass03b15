f553c679c702423a5e8fcc5e5f35b084167f3e12
nooriza <zafar.noori@gmail.com>
2023-01-10 21:45:08 -0500

    Qandulsjldfjsldfj

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Zafar Noori");
         System.out.println("1223434345365");
+        System.out.println("lajsflsjdkf");
     }
 }
